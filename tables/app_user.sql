f4f6eece4b78ebfe3789025d3d70e2169f153c8b
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-06-25 19:43:13 -0400

    Add is_complete boolean

--- tables/app_user.sql
+++ tables/app_user.sql
@@ -3,18 +3,20 @@ CREATE TABLE IF NOT EXISTS app_user (
 	email text NOT NULL,
     phone text,
 	DOB timestamp,
-	
+
 	joined timestamp default now(),
 
 	username text NOT NULL,
 	password text,
 	salt text,
 	roles text NOT NULL,
+	is_complete boolean default false,
 
 	content json,
 	settings json,
 	preferences json,
     statistics json,
+	-- TODO add info json object for localizable data ?
 
 	CONSTRAINT app_user_primary_key PRIMARY KEY (id),
 	CONSTRAINT app_user_username_unique UNIQUE(username),
